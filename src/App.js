2685363406e359b4f913959e14490296c403a251
Fat-Tony <fat_tonyy@icloud.com>
2019-12-02 15:23:22 +0000

    Project Structure

--- src/App.js
+++ src/App.js
@@ -2,12 +2,40 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import Home from "./pages/HomePage";
+import Product from "./pages/ProductsPage";
+import SingleProduct from "./pages/SingleProductPage";
+import Default from "./pages/DefaultPage";
+import Contact from "./pages/ContactPage";
+import Cart from "./pages/CartPage";
+import About from "./pages/AboutPage";
+
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import SideCart from "./components/SideCart";
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello from tech store</h1>
-      </div>
+      <>
+        {/* navbar, sidebar, cart, footer */}
+        <Navbar />
+        <Sidebar />
+        <SideCart />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/product" exact component={Product} />
+          <Route path="/product/:id" exact component={SingleProduct} />
+          <Route path="/cart" exact component={Cart} />
+          <Route component={Default} />
+        </Switch>
+        <Footer />
+      </>
     );
   }
 }
