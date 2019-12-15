9d7c9fb47a56280d55ae2e29d662fd5081e60b1d
Fat-Tony <fat_tonyy@icloud.com>
2019-12-15 10:58:12 +0000

    addTotal method

--- src/context/Context.js
+++ src/context/Context.js
@@ -43,14 +43,19 @@ class ProductProvider extends Component {
     //? Featured products
     let featuredProducts = storeProducts.filter(item => item.featured === true);
 
-    this.setState({
-      storeProducts: storeProducts,
-      filteredProducts: storeProducts,
-      featuredProducts,
-      cart: this.getStorageCart(),
-      singleProduct: this.getStorageProduct(),
-      loading: false
-    });
+    this.setState(
+      {
+        storeProducts: storeProducts,
+        filteredProducts: storeProducts,
+        featuredProducts,
+        cart: this.getStorageCart(),
+        singleProduct: this.getStorageProduct(),
+        loading: false
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   // ! Functionalities Below
@@ -66,10 +71,37 @@ class ProductProvider extends Component {
   };
 
   //get totals
-  getTotals = () => {};
+  getTotals = () => {
+    let subTotal = 0;
+    let cartItems = 0;
+    this.state.cart.forEach(item => {
+      subTotal += item.total;
+      cartItems += item.count;
+    });
+
+    subTotal = parseFloat(subTotal.toFixed(2));
+    let tax = subTotal * 0.2;
+    tax = parseFloat(tax.toFixed(2));
+    let total = subTotal + tax;
+    total = parseFloat(total.toFixed(2));
+    return {
+      cartItems,
+      subTotal,
+      tax,
+      total
+    };
+  };
 
   //add totals
-  addTotals = () => {};
+  addTotals = () => {
+    const totals = this.getTotals();
+    this.setState({
+      cartItems: totals.cartItems,
+      cartSubTotal: totals.subTotal,
+      cartTax: totals.tax,
+      cartTotal: totals.total
+    });
+  };
 
   //sync storage
   syncStorage = () => {};
