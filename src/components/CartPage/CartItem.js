3620dfbe006298579a5d6de1f8c6b36ebffe2a6c
Fat-Tony <fat_tonyy@icloud.com>
2019-12-15 19:00:37 +0000

    cartitem

--- src/components/CartPage/CartItem.js
+++ src/components/CartPage/CartItem.js
@@ -1,9 +1,24 @@
 import React from "react";
+import {
+  FaTrash,
+  FaChevronCircleUp,
+  FaChevronCircleDown
+} from "react-icons/fa";
 
-export default function CartItem() {
+export default function CartItem({
+  cartItem,
+  increment,
+  decrement,
+  removeItem
+}) {
+  const { id, title, price, count, total, image } = cartItem;
   return (
-    <div>
-      <h1>Cart Item</h1>
+    <div className="row mt-5 mt-lg-0 text-capitalize text-center align-items-center">
+      {/* Image */}
+      <div className="col-10 mx-auto col-lg-2 pb-2">
+        <img src={image} width="60" className="img-fluid" alt="product" />
+      </div>
+      {/* end of image */}
     </div>
   );
 }
