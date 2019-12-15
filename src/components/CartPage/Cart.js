abed46f9284027e4df7943e35090e5a7a7918ae0
Fat-Tony <fat_tonyy@icloud.com>
2019-12-15 17:41:46 +0000

    cart columns component

--- src/components/CartPage/Cart.js
+++ src/components/CartPage/Cart.js
@@ -6,10 +6,20 @@ import CartTotal from "./CartTotal";
 
 export default function Cart() {
   return (
-    <>
+    <section className="py-5">
+      {/* Title */}
+      <div className="container">
+        <Title title="your cart items" center />
+      </div>
+
+      {/* Cart Colums */}
       <CartColumns />
+
+      {/* Cart List */}
       <CartList />
+
+      {/* Cart Total */}
       <CartTotal />
-    </>
+    </section>
   );
 }
--- src/components/CartPage/CartColumns.js
+++ src/components/CartPage/CartColumns.js
@@ -2,8 +2,44 @@ import React from "react";
 
 export default function CartColumns() {
   return (
-    <div>
-      <h1>Cart Columns</h1>
+    <div className="container-fluid text-center d-none d-lg-block my-5">
+      <div className="row">
+        {/* single column 1*/}
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Products</p>
+        </div>
+        {/* End of Columns */}
+
+        {/* single column 2 */}
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Name of Product</p>
+        </div>
+        {/* End of Columns */}
+
+        {/* single column 3*/}
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Price</p>
+        </div>
+        {/* End of Columns */}
+
+        {/* single column 4*/}
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Quantity</p>
+        </div>
+        {/* End of Columns */}
+
+        {/* single column 5*/}
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Remove</p>
+        </div>
+        {/* End of Columns */}
+
+        {/* single column 6*/}
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">Total</p>
+        </div>
+        {/* End of Columns */}
+      </div>
     </div>
   );
 }
