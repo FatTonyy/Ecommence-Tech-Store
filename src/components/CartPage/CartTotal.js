210ae087bb82c1bb27646bafc2aac9c1ed92fbc8
Fat-Tony <fat_tonyy@icloud.com>
2019-12-15 17:54:38 +0000

    cart total component

--- src/components/CartPage/CartTotal.js
+++ src/components/CartPage/CartTotal.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { ProductConsumer } from "../../context";
 
 export default function CartTotal() {
   return (
-    <div>
-      <h1>Carttotals</h1>
+    <div className="container">
+      <div className="row">
+        <ProductConsumer>
+          {value => {
+            const { clearCart, cartSubTotal, cartTax, cartTotal } = value;
+
+            return (
+              <div className="col text-title text-center my-4">
+                <button
+                  className="btn btn-outline-danger text-capitalize mb-4"
+                  onClick={clearCart}
+                >
+                  Clear Cart
+                </button>
+
+                <h3>SubTotal : GHC{cartSubTotal}</h3>
+                <h3>Tax : GHC{cartTax}</h3>
+                <h3>Total : GHC{cartTotal}</h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
     </div>
   );
 }
