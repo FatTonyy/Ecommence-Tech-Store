7ecf0c77e4fc93cc6001936da4318503789ea7f2
Fat-Tony <fat_tonyy@icloud.com>
2019-12-15 16:41:06 +0000

    sideCart Component

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,15 +1,42 @@
 import React from "react";
 import { ProductConsumer } from "../context";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function SideCart() {
   return (
     <ProductConsumer>
       {value => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart, cart, cartTotal } = value;
         return (
           <CartWrapper show={cartOpen} onClick={closeCart}>
-            <p>cart items</p>
+            <ul>
+              {cart.map(item => {
+                return (
+                  <li key={item.id} className="cart-item mb-4">
+                    <img width="35" src={`../${item.image}`} alt="cartItem" />
+
+                    <div className="mt-3">
+                      <h6 className="text-uppercase">{item.title}</h6>
+
+                      <h6 className="text-title text-capitalize">
+                        amount : {item.count}
+                      </h6>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <h4 className="text-capitalize text-main">
+              Cart Total : GHC{cartTotal}
+            </h4>
+
+            <div className="text-center my-5">
+              <Link to="/cart" className="main-link">
+                Cart Page
+              </Link>
+            </div>
           </CartWrapper>
         );
       }}
@@ -31,5 +58,14 @@ const CartWrapper = styled.div`
 
   @media (min-width: 576px) {
     width: 20rem;
+  }
+
+  overflow: scroll;
+  padding: 2rem;
+  ul {
+    padding: 0 !important;
+  }
+  .cart-item {
+    list-style-type: none;
   }
 `;
