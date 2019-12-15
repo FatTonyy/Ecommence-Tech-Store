3984a241f5b085779463b5fb46a5502b91ef062d
Fat-Tony <fat_tonyy@icloud.com>
2019-12-15 10:38:27 +0000

    addTotals functionality

--- src/context/Context.js
+++ src/context/Context.js
@@ -69,14 +69,36 @@ class ProductProvider extends Component {
   getTotals = () => {};
 
   //add totals
-  getTotals = () => {};
+  addTotals = () => {};
 
   //sync storage
   syncStorage = () => {};
 
   //Add to Cart
   addToCart = id => {
-    console.log(`add to cart ${id}`);
+    let tempCart = [...this.state.cart];
+    let tempProducts = [...this.state.storeProducts];
+    let tempItem = tempCart.find(item => item.id === id);
+    if (!tempItem) {
+      tempItem = tempProducts.find(item => item.id === id);
+      let total = tempItem.price;
+      let cartItem = { ...tempItem, count: 1, total };
+      tempCart = [...tempCart, cartItem];
+    } else {
+      tempItem.count++;
+      tempItem.total = tempItem.price + tempItem.count;
+      tempItem.total = parseFloat(tempItem.total.toFixed(2));
+    }
+    this.setState(
+      () => {
+        return { cart: tempCart };
+      },
+      () => {
+        this.addTotals();
+        this.syncStorage();
+        this.openCart();
+      }
+    );
   };
 
   //set single product
