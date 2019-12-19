49719ab84943e57b0da2fc8bf9dd782f3bc0e460
Fat-Tony <fat_tonyy@icloud.com>
2019-12-19 13:42:35 +0000

    filter-products component

---
+++ src/components/ProductPage/FilterProducts.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function FilterProducts() {
+  return (
+    <div>
+      <h1>filtering component</h1>
+    </div>
+  );
+}
--- src/components/ProductPage/Products.js
+++ src/components/ProductPage/Products.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ProductConsumer } from "../../context";
 import Title from "../Title";
 import Product from "../Product";
+import FilterProducts from "./FilterProducts";
 
 export default function Products() {
   return (
@@ -15,11 +16,29 @@ export default function Products() {
               {/* title  product section*/}
               <Title center title="our products" />
 
+              {/* filteredProducts section */}
+              <FilterProducts />
+              {/* total count under filtered section */}
+              <div className="row">
+                <div className="col-10 mx-auto">
+                  <h6 className="text-title">
+                    total products : {filteredProducts.length}
+                  </h6>
+                </div>
+              </div>
+              {/* end of filteredProducts section */}
+
               {/* Products */}
               <div className="row py-5">
-                {filteredProducts.map(product => {
-                  return <Product key={product.id} product={product} />;
-                })}
+                {filteredProducts.length === 0 ? (
+                  <div className="col text-title text-center">
+                    sorry no items matched your search
+                  </div>
+                ) : (
+                  filteredProducts.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  })
+                )}
               </div>
             </div>
           </section>
